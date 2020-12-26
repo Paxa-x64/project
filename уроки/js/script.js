6c6dd63b7e946644b2f74582f3d29eaa4f8a41c2
Pavel <vanyax65@gmail.com>
2020-12-26 20:01:14 +0200

    film

--- уроки/js/script.js
+++ уроки/js/script.js
@@ -1,1 +1,20 @@
-console.log(1);
+// console.log(1);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фольмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фольмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
